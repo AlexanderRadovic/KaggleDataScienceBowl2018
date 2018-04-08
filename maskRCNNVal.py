35adc255b22300af3b1cc52cc76a3713733fb479
Alexander <a.radovic@gmail.com>
2018-04-08 23:09:24 +0000

    Small update to training epochs, new script to check performance

--- maskRCNNVal.py
+++ maskRCNNVal.py
@@ -1,4 +1,4 @@
-#macro to train a mask rnn for Nuclei segmentation
+#macro to check performance on validation set, making plots to help explore behaviour
 #thanks to:
 #-the github repo Mask-RCNN
 
@@ -37,6 +37,17 @@ COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
 if not os.path.exists(COCO_MODEL_PATH):
         utils.download_trained_weights(COCO_MODEL_PATH)
 
+def get_ax(rows=1, cols=1, size=8):
+        """Return a Matplotlib Axes array to be used in
+        all visualizations in the notebook. Provide a
+        central point to control graph sizes.
+        
+        Change the default size attribute to control the size
+        of rendered images
+        """
+        _, ax = plt.subplots(rows, cols, figsize=(size*cols, size*rows))
+        return ax
+
 class NucleiConfig(Config):
     """Configuration for training on the kaggle nuclei
     Derives from the base Config class and overrides values specific
@@ -69,14 +80,7 @@ class NucleiConfig(Config):
     STEPS_PER_EPOCH = 200
         
     # use small validation steps since the epoch is small
-    VALIDATION_STEPS = 10
-
-config = NucleiConfig()
-config.display()
-
-dataset_train = NucleiDatasetTrain()
-dataset_train.load_nuclei()
-dataset_train.prepare()
+    VALIDATION_STEPS = 5
 
 dataset_val = NucleiDatasetTest()
 dataset_val.load_nuclei()
@@ -88,45 +92,49 @@ dataset_val.prepare()
 #         mask, class_ids = dataset_val.load_mask(i)
 #         visualize.display_top_masks(image, mask, class_ids,['background','nuclei'])
         
+class InferenceConfig(NucleiConfig):
+        GPU_COUNT = 1
+        IMAGES_PER_GPU = 1
+        
+inference_config = InferenceConfig()
 
-# Create model in training mode
-model = modellib.MaskRCNN(mode="training", config=config,
+# Recreate the model in inference mode
+model = modellib.MaskRCNN(mode="inference",
+                          config=inference_config,
                           model_dir=MODEL_DIR)
 
-# Which weights to start with?
-init_with = "coco"  # imagenet, coco, or last
+# Get path to saved weights
+# Either set a specific path or find last trained weights
+# model_path = os.path.join(ROOT_DIR, ".h5 file name here")
+model_path = 'Mask_RCNN/logs/mask_rcnn_nuclei.h5'#model.find_last()[1]
 
-if init_with == "imagenet":
-            model.load_weights(model.get_imagenet_weights(), by_name=True)
-elif init_with == "coco":
-            # Load weights trained on MS COCO, but skip layers that
-            # are different due to the different number of classes
-            # See README for instructions to download the COCO weights
-            model.load_weights(COCO_MODEL_PATH, by_name=True,
-                               exclude=["mrcnn_class_logits", "mrcnn_bbox_fc",
-                                        "mrcnn_bbox", "mrcnn_mask"])
-#elif init_with == "last":
-            # Load the last model you trained and continue training
+# Load trained weights (fill in path to trained weights here)
+assert model_path != "", "Provide path to trained weights"
+print("Loading weights from ", model_path)
+model.load_weights(model_path, by_name=True)
 
 
+# Load and display a series of test images
+for i in range(0,10):
+        image_id = i
+        original_image, image_meta, gt_class_id, gt_bbox, gt_mask =\
+                modellib.load_image_gt(dataset_val, inference_config,
+                                       image_id, use_mini_mask=False)
 
-# Train the head branches
-# Passing layers="heads" freezes all layers except the head
-# layers. You can also pass a regular expression to select
-# which layers to train by name pattern.
-model.train(dataset_train, dataset_val,
-            learning_rate=config.LEARNING_RATE,
-            epochs=2,
-            layers='heads')
+        log("original_image", original_image)
+        log("image_meta", image_meta)
+        log("gt_class_id", gt_class_id)
+        log("gt_bbox", gt_bbox)
+        log("gt_mask", gt_mask)
 
-# Fine tune all layers
-# Passing layers="all" trains all layers. You can also
-# pass a regular expression to select which layers to
-# train by name pattern.
-model.train(dataset_train, dataset_val,
-                        learning_rate=config.LEARNING_RATE / 10,
-                        epochs=30,
-                        layers="all")
+        #true masks
+        visualize.display_instances(original_image, gt_bbox, gt_mask, gt_class_id,
+                            dataset_val.class_names, figsize=(8, 8))
 
+        #reconstructed masks
+        results = model.detect([original_image], verbose=1)
+        r = results[0]
+        visualize.display_instances(original_image, r['rois'], r['masks'], r['class_ids'],
+                                    dataset_val.class_names, r['scores'], ax=get_ax())
 
 
