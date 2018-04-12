8b1398ed72030199be7bf5bec3412c67e75ce368
AlexanderRadovic <a.radovic@gmail.com>
2018-04-12 09:17:02 -0500

    Changes to support making alternative sub-images out of the larger training examples.

--- convertToNPY.py
+++ convertToNPY.py
@@ -27,6 +27,7 @@ IMG_CHANNELS = 3
 TRAIN_PATH = 'stage1_train/'
 TEST_PATH = 'stage1_test/'
 augment = False
+normalize = False
 
 warnings.filterwarnings('ignore', category=UserWarning, module='skimage')
 seed = 42
@@ -48,8 +49,8 @@ for n, id_ in tqdm(enumerate(train_ids), total=len(train_ids)):
         
         #acquire original image and resize so all images match
         path = TRAIN_PATH + id_
-        img = imread(path + '/images/' + id_ + '.png')[:,:,:IMG_CHANNELS]                                
-        img = resize(img, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
+        imgOrig = imread(path + '/images/' + id_ + '.png')[:,:,:IMG_CHANNELS]                                
+        img = resize(imgOrig, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
 
 	#acquire original mask and resize so all images match
         mask = np.zeros((IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.bool)
@@ -64,39 +65,64 @@ for n, id_ in tqdm(enumerate(train_ids), total=len(train_ids)):
 	Y_train.append(mask)
 
 	if augment == True:
-		for j in range(0, 10):
-			cloneImg=img
-			cloneMask=mask
-			
-		#randomly flip x axis
-			if random() > 0.5:
-				cloneImg=np.flip(cloneImg, 0)
-				cloneMask=np.flip(cloneMas, 0)
-			
-		#randomly flip y axis 
-			if random() > 0.5:
-				cloneImg=np.flip(cloneImg, 1)
-				cloneMask=np.flip(cloneMask, 1)
+		#create alternative mask/image pairs out of the larger training examples
+		if imgOrig.shape[0]>IMG_HEIGHT or imgOrig.shape[0]>IMG_WIDTH:
+			for i in range(ceil(IMG_HEIGHT/imgOrig.shape[0])):
+				for j in range(ceil(IMG_HEIGHT/imgOrig.shape[0])):
+					subImg=imgOrig[i*IMGHEIGHT:(i+1)*IMGHEIGHT,j*IMGWIDTH:(j+1)*IMGWIDTH,:]
 
-		#random rotation
-			if random() > 0.5:
-				rotAngle=360*random()
-				cloneImg=rotate(cloneImg, rotAngle)
-				cloneMask=rotate(cloneMask, rotAngle)
+					subMask = np.zeros((IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.bool)
+					for mask_file in next(os.walk(path + '/masks/'))[2]:
+						mask_ = imread(path + '/masks/' + mask_file)
+						mask_ = mask_[i*IMGHEIGHT:(i+1)*IMGHEIGHT,j*IMGWIDTH:(j+1)*IMGWIDTH,:]
+						mask_ = np.expand_dims(resize(mask_, (IMG_HEIGHT, IMG_WIDTH), mode='constant',preserve_range=True), axis=-1)
+						mask = np.maximum(mask, mask_)
 
-		#randomly shift color palette
-			if random() > 0.5:
-				cloneImg=rgb2hsv(cloneImg)
+					subImg=subImg.astype(np.uint8)
+					subMask=subMask.astype(np.bool)
+
+					X_train.append(subImg)
+					Y_train.append(subMask)
+
+# 		for j in range(0, 10):
+# 			cloneImg=img
+# 			cloneMask=mask
 			
-		#randomly shift color palette
-			if random() > 0.5:
-				cloneImg=hsv2rgb(cloneImg)
+# 		#randomly flip x axis
+# 			if random() > 0.5:
+# 				cloneImg=np.flip(cloneImg, 0)
+# 				cloneMask=np.flip(cloneMas, 0)
+			
+# 		#randomly flip y axis 
+# 			if random() > 0.5:
+# 				cloneImg=np.flip(cloneImg, 1)
+# 				cloneMask=np.flip(cloneMask, 1)
 
-			X_train.append(cloneImg)
-			Y_train.append(cloneMask)
+# 		#random rotation
+# 			if random() > 0.5:
+# 				rotAngle=360*random()
+# 				cloneImg=rotate(cloneImg, rotAngle)
+# 				cloneMask=rotate(cloneMask, rotAngle)
+
+# 		#randomly shift color palette
+# 			if random() > 0.5:
+# 				cloneImg=rgb2hsv(cloneImg)
+			
+# 		#randomly shift color palette
+# 			if random() > 0.5:
+# 				cloneImg=hsv2rgb(cloneImg)
+
+# 			X_train.append(cloneImg)
+# 			Y_train.append(cloneMask)
 
 	
 	i=i+1
+
+if normalize == True: 
+	for i in range(len(X_train)):
+		div = X_train[i].max(axis=tuple(np.arange(1,len(X_train[i].shape))), keepdims=True) 
+		div[div < 0.01*X_train[i].mean()] = 1. # protect against too small pixel intensities
+		X_train[i] = X_train[i].astype(np.float32)/div
 
 np.save('inputImages.npy',np.stack(X_train))
 np.save('inputMask.npy',np.stack(Y_train))
